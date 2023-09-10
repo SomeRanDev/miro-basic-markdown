feba903cb17c2a85443ff655c530c3c6defec1fc
Robert Borghese <28355157+SomeRanDev@users.noreply.github.com>
2023-09-09 20:13:14 -0400

    Cleanup and redo description for app modal

--- src/app.tsx
+++ src/app.tsx
@@ -1,22 +1,12 @@
 import * as React from 'react';
 import {createRoot} from 'react-dom/client';
 
-// async function addSticky() {
-//	 const stickyNote = await miro.board.createStickyNote({
-//		 content: 'Hello, World!',
-//	 });
-
-//	 await miro.board.viewport.zoomTo(stickyNote);
-// }
-
 const App: React.FC = () => {
-//	 React.useEffect(() => {
-//		 addSticky();
-//	 }, []);
-
 	return (
 		<div className="grid wrapper">
-			Welcome to SomeRanDev's Utils!
+			Welcome to Basic Markdown.<br>
+			<br>
+			Check out the <a href="https://github.com/SomeRanDev/miro-basic-markdown">source code on Github.</a>
 		</div>
 	);
 };
