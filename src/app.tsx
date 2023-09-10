8c20a532179bda9121f7c9fe8fdf3ad6baf20f9d
Robert Borghese <28355157+SomeRanDev@users.noreply.github.com>
2023-09-09 20:34:13 -0400

    Fixed unclosed br html tag

--- src/app.tsx
+++ src/app.tsx
@@ -4,8 +4,8 @@ import {createRoot} from 'react-dom/client';
 const App: React.FC = () => {
 	return (
 		<div className="grid wrapper">
-			Welcome to Basic Markdown.<br>
-			<br>
+			Welcome to Basic Markdown.<br/>
+			<br/>
 			Check out the <a href="https://github.com/SomeRanDev/miro-basic-markdown">source code on Github.</a>
 		</div>
 	);
