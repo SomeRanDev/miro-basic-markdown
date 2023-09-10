d3cf834e0e4dbefc67e38c9fb4d3ae2ecf13c181
Robert Borghese <28355157+SomeRanDev@users.noreply.github.com>
2023-09-09 23:56:17 -0400

    Add tutorial to application button

--- src/app.tsx
+++ src/app.tsx
@@ -3,10 +3,49 @@ import {createRoot} from 'react-dom/client';
 
 const App: React.FC = () => {
 	return (
-		<div className="grid wrapper">
-			Welcome to Basic Markdown.<br/>
-			<br/>
+		<div style={{ display: "flex", flexDirection: "column" }}>
+			Welcome to Basic Markdown!
+			<br/><br/>
 			Check out the <a href="https://github.com/SomeRanDev/miro-basic-markdown">source code on Github.</a>
+			<h2>Instructions</h2>
+			Simply run the "Enable Markdown" action on any Text, Shape, or Sticky Note.
+			<br/><br/>
+			It will now convert between raw Markdown syntax and visual HTML when selected and unselected respectively.
+			<h2>Supported Syntax</h2>
+			<table>
+				<tr>
+					<th>Syntax</th>
+					<th>Output</th>
+				</tr>
+				<tr>
+					<td>_underline_</td>
+					<td><u>underline</u></td>
+				</tr>
+				<tr>
+					<td>*italic*</td>
+					<td><i>italic</i></td>
+				</tr>
+				<tr>
+					<td>**bold**</td>
+					<td><b>bold</b></td>
+				</tr>
+				<tr>
+					<td>~strikethrough~</td>
+					<td><s>strikethrough</s></td>
+				</tr>
+				<tr>
+					<td>[ ] unchecked</td>
+					<td>🔲 unchecked</td>
+				</tr>
+				<tr>
+					<td>[x] checked</td>
+					<td>✅ checked</td>
+				</tr>
+				<tr>
+					<td>* bullet point</td>
+					<td>• bullet point</td>
+				</tr>
+			</table>
 		</div>
 	);
 };
